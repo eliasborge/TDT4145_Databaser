ff8e0d6b3521240b4e5aa213605ceb6828bbf296
Elias Borge Svinø <eliasbosv@gmail.com>
2022-03-20 17:43:58 +0100

    temp push

--- src/UI.py
+++ src/UI.py
@@ -1,11 +1,22 @@
+import sqlite3
+
+con = sqlite3.connect("KaffeDB.db")
+
+
 
 def lagBruker():
     print("Fyll inn feltene:")
-    brukerEpost = input("/n Epost: ")
-    brukerFornavn = input("/n Fornavn: ")
-    brukerEtternavn = input("/n Etternavn: ")
-    brukerPassord = input("/n Passord: ")
+    brukerEpost = input("\nEpost: ")
+    brukerFornavn = input("\nFornavn: ")
+    brukerEtternavn = input("\nEtternavn: ")
+    brukerPassord = input("\nPassord: ")
     #Koble/registrere til databasen
+    
+    con.execute(''' 
+        INSERT INTO Bruker(BrukerEpost,BrukerPassord,Fornavn,Etternavn)
+        VALUES(%d,%d,%d,%d)
+    ''',brukerEpost,brukerPassord,brukerFornavn,brukerEtternavn)
+lagBruker()
 
 def login():
     print("Logg inn eller registrer deg")
@@ -20,11 +31,31 @@ def login():
         #registrer
 
 
-
 def starter():
-    print("Velkommen til KaffeDB")
+    int("Velkommen til KaffeDB")
     print("Meny: ")
     print("Trykk på K for å legge til en kaffesmaking")
     print("Trykk på S for å søke")
     print("Trykk på D for å slette en kaffesmaking")
+    svar = input("Velg 1 alternativ: ")
+
+    if(svar == "gdkdk"):
+        søk()
+    
+
+def  kaffeSmaking():
+    print("Ny kaffesmaking")
+    rangering = input("\nRangering: ")
+    smaksdato = input("\nSmaksdato: ")
+    smaksnotater = input("\nSmaksnotat: ")
+    
+def søk():
+    hei = 0
+    #Navn
+    #
+
+
+def slett():
+    slett = 0
+
 
---
+++ src/user.py
@@ -0,0 +1,9 @@
+class User:
+
+    def  __init__(self,epost,passord,fornavn,etternavn):
+        self.epost = epost 
+        self.passord = passord
+        self.fornavn = fornavn
+        self.etternavn = etternavn
+
+    
