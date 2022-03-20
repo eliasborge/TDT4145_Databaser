45225059905f62365ded8497c5de968ecd184a09
Elias Borge Svinø <eliasbosv@gmail.com>
2022-03-20 17:09:21 +0100

    Add ui file

---
+++ src/UI.py
@@ -0,0 +1,25 @@
+
+def lagBruker():
+    print()
+
+def login():
+    print("Logg inn eller registrer deg")
+    
+    brukersvar = input("Trykk på B for å lage en bruker eller trykk på L for å logge inn")
+
+    if(brukersvar == "B"):
+        #Lag bruker
+        lagBruker()
+    else:   
+        #registrer
+
+
+
+def input():
+    print("Velkommen til KaffeDB")
+    print("Meny: ")
+    print("Trykk på K for å legge til en kaffesmaking")
+    print("Trykk på S for å søke")
+    print("Trykk på D for å slette en kaffesmaking")
+
+    
